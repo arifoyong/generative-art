b4458a8d5f584d398ad2613c28c3c58a487ecbb4
arifoyong <arifoyong@gmail.com>
2022-02-13 21:52:05 +0800

    Modified perlin2.js

---
+++ pages/test/perlin2.js
@@ -0,0 +1,50 @@
+// Reference: https://www.youtube.com/watch?v=Qf4dIN99e2w&list=PLRqwX-V7Uu6bgPNQAdxQZpJuJCjeOr7VD
+import { useEffect, useRef } from "react";
+
+
+const Perlin2 = () => {
+  const myRef = useRef(null)
+  let points = []
+  let mult = 0.005
+
+  const Sketch = p => {
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight).parent(myRef.current)
+      p.background(30)
+      // p.noLoop()
+
+      const density = 10
+      const space = p.windowWidth / density
+      for (let i=0; i<p.windowWidth; i+=space) {
+        for (let j=0; j<p.windowHeight; j+=space) {
+          let vec = p.createVector(i,j)
+          points.push(vec)
+        }
+      }
+    }
+  
+    p.draw = () => {
+      p.noStroke()
+      p.fill(255)
+
+      for (let i=0; i<points.length; i++) {
+        let angle = p.map(p.noise(points[i].x*mult, points[i].y*mult), 0,1, 0, 720)
+        
+        points[i].add(p.createVector(p.cos(angle), p.sin(angle)))
+        p.ellipse(points[i].x, points[i].y, 5)
+      }
+     
+    }
+  
+
+  }
+ 
+  useEffect(() => {
+    const p5 = require("p5")
+    new p5(Sketch)
+  }, [])
+ 
+ return <div ref={myRef}></div>
+}
+ 
+export default Perlin2;
--- pages/test/perlin_noise.js
+++ pages/test/perlin_noise.js
@@ -1,48 +1,28 @@
 // Reference: https://www.youtube.com/watch?v=Qf4dIN99e2w&list=PLRqwX-V7Uu6bgPNQAdxQZpJuJCjeOr7VD
 import { useEffect, useRef } from "react";
 
+// const TwoD_Perlin = (p) => {
+//   const inc = 0.1
+//   const sq = 20
+//   let noiseY = 0
 
-const drawFunc = (p) => {
-  p.noFill()
-  for (let i=0; i<p.windowWidth; i+=10) {
-    const j = p.random(50,150) 
-    p.vertex(i,j)
-    p.ellipse(i,j, 2)
-  }
-  p.endShape()
-
-  p.stroke(0,255,0)
-  p.beginShape()
-  for (let i=0; i<p.windowWidth; i+=10) {
-    const j = p.noise(i/100) *100 + 50
-    p.vertex(i,j)
-    p.ellipse(i,j, 2)
-  }
-  p.endShape()
-}
-
-const TwoD_Perlin = (p) => {
-  const inc = 0.1
-  const sq = 20
-  let noiseY = 0
-
-  for (let x=0; x<p.windowWidth; x+=sq) {
-    let noiseX = 0
-    for (let y=0; y<p.windowHeight; y+=sq) {
-      const color = p.noise(noiseX, noiseY) * 255
-      p.fill(color)
-      p.rect(x,y, sq)
-      noiseX += inc
-    }
-    noiseY += inc
-  }
-}
+//   for (let x=0; x<p.windowWidth; x+=sq) {
+//     let noiseX = 0
+//     for (let y=0; y<p.windowHeight; y+=sq) {
+//       const color = p.noise(noiseX, noiseY) * 255
+//       p.fill(color)
+//       p.rect(x,y, sq)
+//       noiseX += inc
+//     }
+//     noiseY += inc
+//   }
+// }
 
 const TwoD_Perlin_Line = (p) => {
-  
   const inc = 0.1
-  const sq = 20
+  const sq = 10
   let noiseY = 0
+  // p.stroke(p.random(0,255), p.random(0,255), p.random(0,255))
   
   for (let x=0; x<p.windowWidth; x+=sq) {
     let noiseX = 0
@@ -50,7 +30,7 @@ const TwoD_Perlin_Line = (p) => {
       const angle = p.noise(noiseX, noiseY) * 2*Math.PI
       p.push()
         p.translate(x,y)
-        // p.stroke(p.random(0,255), p.random(0,255), p.random(0,255))
+        
         const v = p.constructor.Vector.fromAngle(angle)
         p.line(0,0 ,v.x * sq, v.y * sq )
       p.pop()
@@ -58,18 +38,17 @@ const TwoD_Perlin_Line = (p) => {
     }
     noiseY += inc
   }
-
-
 }
 
-const ImperfectCircle = () => {
+
+const FlowField = () => {
   const myRef = useRef(null)
 
   const Sketch = p => {
     p.setup = () => {
       p.createCanvas(p.windowWidth, p.windowHeight).parent(myRef.current)
       p.background(255)
-      // p.noLoop()
+      p.noLoop()
     }
   
     p.draw = () => TwoD_Perlin_Line(p)
@@ -85,4 +64,4 @@ const ImperfectCircle = () => {
  return <div ref={myRef}></div>
 }
  
-export default ImperfectCircle;
+export default FlowField;
